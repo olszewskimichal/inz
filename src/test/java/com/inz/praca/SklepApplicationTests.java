5343dcee4fb2d883929d0c2e7cee312d0ddee55c
olszewskimichal <michalolsz@mat.umk.pl>
2016-12-18 13:47:06 +0100

    100% pokrycia - czas wziac sie za cos powazniejszego

--- src/test/java/com/inz/praca/SklepApplicationTests.java
+++ src/test/java/com/inz/praca/SklepApplicationTests.java
@@ -1,16 +1,13 @@
 package com.inz.praca;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class SklepApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		SklepApplication sklepApplication=new SklepApplication();
+		sklepApplication.main(new String[]{"aa"});
 	}
 
 }
