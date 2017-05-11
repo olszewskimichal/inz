66e7235cade5f61dd2772bec0e38b9b9008faab6
olszewskimichal <michalolsz@mat.umk.pl>
2017-05-11 19:44:18 +0200

    testujemy joby travisa

--- src/test/java/com/inz/praca/SklepApplicationTests.java
+++ src/test/java/com/inz/praca/SklepApplicationTests.java
@@ -1,9 +1,11 @@
 package com.inz.praca;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
+@Category(IntegrationTest.class)
 public class SklepApplicationTests {
 
     @Test
